70243c60362fe10a6fcbea331fe8eda7fca24c29
donskytech <donsky.iot.tech@gmail.com>
2023-04-06 14:30:12 +0800

    Fingerprint with DB Interface

---
+++ esp32-projects/esp32-r307-fingerprint-door-lock-db-interface/src/back.cpp
@@ -0,0 +1,279 @@
+// #include <Arduino.h>
+// #include <Adafruit_Fingerprint.h>
+// #include <WiFi.h>
+// #include <AsyncTCP.h>
+// #include "SPIFFS.h"
+// #include <ESPAsyncWebServer.h>
+
+// // Change this to match your network SSID
+// const char *ssid = "<YOUR_WIFI_SSID>";
+// const char *password = "<YOUR_WIFI_PASSWORD>";
+
+// // AsyncWebserver runs on port 80 and the asyncwebsocket is initialize at this point also
+// AsyncWebServer server(80);
+// AsyncEventSource events("/events");
+
+// HardwareSerial serialPort(2); // use UART2
+// Adafruit_Fingerprint finger = Adafruit_Fingerprint(&serialPort);
+
+// //  Forward declaration
+// uint8_t getFingerprintID();
+// void showNotAllowed();
+
+// // Function called when resource is not found on the server
+// void notFound(AsyncWebServerRequest *request)
+// {
+//   request->send(404, "text/plain", "Not found");
+// }
+// // interval to read the fingerprint sensor
+// unsigned long previousMillis = 0;
+// const long readInterval = 1000;
+
+// // interval when to close the door lock
+// bool isOpen = false;
+// const long closeInterval = 5000;
+// unsigned long previousOpenMillis = 0;
+
+// // Relay Pin
+// const int RELAY_PIN = 23;
+
+// // Buzzer
+// const int buzzerPin = 22;
+
+// void setup()
+// {
+//   Serial.begin(9600);
+//   while (!Serial)
+//     ; // For Yun/Leo/Micro/Zero/...
+//   delay(100);
+
+//   // Connect to your wifi
+//   WiFi.mode(WIFI_STA);
+//   WiFi.begin(ssid, password);
+//   if (WiFi.waitForConnectResult() != WL_CONNECTED)
+//   {
+//     Serial.printf("WiFi Failed!\n");
+//     return;
+//   }
+
+//   Serial.print("IP Address: ");
+//   Serial.println(WiFi.localIP());
+
+//   // Initialize SPIFFS
+//   if (!SPIFFS.begin(true))
+//   {
+//     Serial.println("An Error has occurred while mounting SPIFFS");
+//     return;
+//   }
+
+//   // setup Server Sent Event
+//   events.onConnect([](AsyncEventSourceClient *client)
+//                    {
+//     if (client->lastId())
+//     {
+//       Serial.printf("Client reconnected! Last message ID that it gat is: %u\n", client->lastId());
+//     } });
+
+//   // attach AsyncEventSource
+//   server.addHandler(&events);
+
+//   server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
+//             {
+//               Serial.println("Requesting index page...");
+//               request->send(SPIFFS, "/index.html", "text/html", false); });
+//   server.serveStatic("/css", SPIFFS, "/css/");
+//   server.serveStatic("/js", SPIFFS, "/js/");
+
+//   // On Not Found
+//   server.onNotFound(notFound);
+
+//   // Start server
+//   server.begin();
+
+//   // set the data rate for the fingerprint sensor serial port
+//   finger.begin(57600);
+//   delay(5);
+//   if (finger.verifyPassword())
+//   {
+//     Serial.println("Found fingerprint sensor!");
+//   }
+//   else
+//   {
+//     Serial.println("Did not find fingerprint sensor :(");
+//     while (1)
+//     {
+//       delay(1);
+//     }
+//   }
+
+//   Serial.println(F("Reading sensor parameters"));
+//   finger.getParameters();
+//   Serial.print(F("Status: 0x"));
+//   Serial.println(finger.status_reg, HEX);
+//   Serial.print(F("Sys ID: 0x"));
+//   Serial.println(finger.system_id, HEX);
+//   Serial.print(F("Capacity: "));
+//   Serial.println(finger.capacity);
+//   Serial.print(F("Security level: "));
+//   Serial.println(finger.security_level);
+//   Serial.print(F("Device address: "));
+//   Serial.println(finger.device_addr, HEX);
+//   Serial.print(F("Packet len: "));
+//   Serial.println(finger.packet_len);
+//   Serial.print(F("Baud rate: "));
+//   Serial.println(finger.baud_rate);
+
+//   finger.getTemplateCount();
+
+//   if (finger.templateCount == 0)
+//   {
+//     Serial.print("Sensor doesn't contain any fingerprint data. Please run the 'enroll' example.");
+//   }
+//   else
+//   {
+//     Serial.println("Waiting for valid finger...");
+//     Serial.print("Sensor contains ");
+//     Serial.print(finger.templateCount);
+//     Serial.println(" templates");
+//   }
+
+//   // initialize Relay digital pin as an output.
+//   pinMode(RELAY_PIN, OUTPUT);
+//   digitalWrite(RELAY_PIN, HIGH);
+
+//   // configure buzzer and led pin
+//   pinMode(buzzerPin, OUTPUT);
+// }
+
+// void loop()
+// {
+//   unsigned long currentMillis = millis();
+//   // Close the door lock after 5 seconds
+//   if (isOpen && currentMillis - previousOpenMillis >= closeInterval)
+//   {
+//     isOpen = false;
+//     Serial.println("Closing the door lock!");
+//     digitalWrite(RELAY_PIN, HIGH);
+//   }
+
+//   // save the last time we read the fingerprint sensor
+//   if (!isOpen && currentMillis - previousMillis >= readInterval)
+//   {
+//     previousMillis = currentMillis;
+//     uint8_t result = getFingerprintID();
+//     switch (result)
+//     {
+//     case FINGERPRINT_NOFINGER:
+//       Serial.println("Scan your fingerprint");
+//       events.send("Scan your fingerprint", "noFingerprint", millis());
+//       break;
+//     case FINGERPRINT_OK:
+//       Serial.println("Access Granted..opening door lock!");
+//       events.send("Access Granted", "accessGranted", millis());
+//       previousOpenMillis = millis();
+//       isOpen = true;
+//       digitalWrite(RELAY_PIN, LOW);
+//       break;
+//     case FINGERPRINT_NOTFOUND:
+//       Serial.println("Access Denied");
+//       events.send("Access Denied", "accessDenied", millis());
+//       showNotAllowed();
+//       delay(2000);
+//       break;
+//     case FINGERPRINT_PACKETRECIEVEERR:
+//     case FINGERPRINT_IMAGEFAIL:
+//     case FINGERPRINT_IMAGEMESS:
+//     case FINGERPRINT_FEATUREFAIL:
+//     case FINGERPRINT_INVALIDIMAGE:
+//       Serial.println("Error in Fingerprint Scan!");
+//       events.send("Unknown Error", "unknownError", millis());
+//       break;
+//     default:
+//       Serial.println("Unknown Error!");
+//       break;
+//     }
+//   }
+// }
+
+// uint8_t getFingerprintID()
+// {
+//   uint8_t p = finger.getImage();
+//   switch (p)
+//   {
+//   case FINGERPRINT_OK:
+//     Serial.println("Image taken");
+//     break;
+//   case FINGERPRINT_NOFINGER:
+//     return p;
+//   case FINGERPRINT_PACKETRECIEVEERR:
+//     Serial.println("Communication error");
+//     return p;
+//   case FINGERPRINT_IMAGEFAIL:
+//     Serial.println("Imaging error");
+//     return p;
+//   default:
+//     Serial.println("Unknown error");
+//     return p;
+//   }
+
+//   // OK success!
+
+//   p = finger.image2Tz();
+//   switch (p)
+//   {
+//   case FINGERPRINT_OK:
+//     Serial.println("Image converted");
+//     break;
+//   case FINGERPRINT_IMAGEMESS:
+//     Serial.println("Image too messy");
+//     return p;
+//   case FINGERPRINT_PACKETRECIEVEERR:
+//     Serial.println("Communication error");
+//     return p;
+//   case FINGERPRINT_FEATUREFAIL:
+//     Serial.println("Could not find fingerprint features");
+//     return p;
+//   case FINGERPRINT_INVALIDIMAGE:
+//     Serial.println("Could not find fingerprint features");
+//     return p;
+//   default:
+//     Serial.println("Unknown error");
+//     return p;
+//   }
+
+//   // OK converted!
+//   p = finger.fingerSearch();
+//   if (p == FINGERPRINT_OK)
+//   {
+//     Serial.println("Found a print match!");
+//     // found a match!
+//     Serial.print("Found ID #");
+//     Serial.print(finger.fingerID);
+//     Serial.print(" with confidence of ");
+//     Serial.println(finger.confidence);
+
+//     return p;
+//   }
+//   else if (p == FINGERPRINT_PACKETRECIEVEERR)
+//   {
+//     Serial.println("Communication error");
+//     return p;
+//   }
+//   else if (p == FINGERPRINT_NOTFOUND)
+//   {
+//     Serial.println("Did not find a match");
+//     return p;
+//   }
+//   else
+//   {
+//     Serial.println("Unknown error");
+//     return p;
+//   }
+// }
+
+// void showNotAllowed()
+// {
+//   digitalWrite(buzzerPin, HIGH);
+//   delay(1000);
+//   digitalWrite(buzzerPin, LOW);
+// }
