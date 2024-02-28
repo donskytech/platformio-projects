30b06ff28558dc683cef7d9bc306a67df55a1dfd
donskytech <donsky.iot.tech@gmail.com>
2024-02-28 11:16:24 +0800

    clear comments

--- esp32-projects/esp32-mqtt-node-red-control/src/main.cpp
+++ esp32-projects/esp32-mqtt-node-red-control/src/main.cpp
@@ -72,17 +72,6 @@ void loop()
     reconnect();
   }
   client.loop();
-  // for (pos = 0; pos <= 180; pos += 1)
-  // { // goes from 0 degrees to 180 degrees
-  //   // in steps of 1 degree
-  //   servoMotor.write(pos); // tell servo to go to position in variable 'pos'
-  //   delay(20);             // waits 15ms for the servo to reach the position
-  // }
-  // for (pos = 180; pos >= 0; pos -= 1)
-  // {                        // goes from 180 degrees to 0 degrees
-  //   servoMotor.write(pos); // tell servo to go to position in variable 'pos'
-  //   delay(20);             // waits 15ms for the servo to reach the position
-  // }
 }
 
 void setupWifi()
